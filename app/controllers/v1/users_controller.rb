10c7f3e680b51ffec8383b096c884f9942480594
Richard Song <richardsong18@gmail.com>
2018-04-15 15:30:59 -0500

    Add create user action on backend

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,16 @@
+class V1::UsersController < ApplicationController
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email], 
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+      )
+
+    if user.save 
+      render json: {message: "User successfully created!"}, status: :created 
+    else
+      render json: {errors: user.errors.full_messages}, status: :bad_request
+    end
+  end 
+end

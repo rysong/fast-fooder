e2cdd26ec2d56d83b17b523e3df9a4f9f007241b
Richard Song <richardsong18@gmail.com>
2018-05-10 20:04:02 -0500

    add image attribute to meals and show on frontend

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,12 +1,14 @@
 class Meal < ApplicationRecord
   belongs_to :restaurant
-  has_many :user_meals 
+  has_many :user_meals
+
 
   def as_json
     {
       id: id,
       description: description,
       upvotes: upvotes,
+      image: image, 
       user_meals: user_meals.as_json,
       new_user_meal_text: ""
     }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180510012131) do
+ActiveRecord::Schema.define(version: 20180511005543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180510012131) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "upvotes"
+    t.string "image"
   end
 
   create_table "restaurants", force: :cascade do |t|

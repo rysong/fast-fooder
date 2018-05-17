a19bc7ec35ccefc4c6c982527b018c04d32b21aa
Richard Song <richardsong18@gmail.com>
2018-05-16 21:32:41 -0500

    Seeds and change order of description and name

--- db/seeds.rb
+++ db/seeds.rb
@@ -65,6 +65,48 @@ restaurant = Restaurant.new(
   speed_of_service: 7
   )
 restaurant.save 
+
+#restaurant 5
+restaurant = Restaurant.new(
+  name: "Chipotle Mexican Grill", 
+  address: "6 W Grand Ave, Chicago, IL 60611",
+  logo:"https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Chipotle_Mexican_Grill_logo.svg/200px-Chipotle_Mexican_Grill_logo.svg.png",
+  primary_image: "https://b.zmtcdn.com/data/pictures/9/16733319/c2bbf5a747d71164e23fa0f2ada172f3.jpg", 
+  speed_of_service: 6
+  )
+restaurant.save
+
+#restaurant 6
+restaurant = Restaurant.new(
+  name: "Taco Bell", 
+  address: "100 W Randolph St, Chicago, IL 60601",
+  logo:"https://upload.wikimedia.org/wikipedia/en/thumb/b/b3/Taco_Bell_2016.svg/150px-Taco_Bell_2016.svg.png",
+  primary_image: "https://s3-media1.fl.yelpcdn.com/bphoto/cmpPzdcUiYwezfZnmHiT8w/o.jpg", 
+  speed_of_service: 7
+  )
+restaurant.save
+
+#restaurant 7
+restaurant = Restaurant.new(
+  name: "Al's Beef", 
+  address: "169 W Ontario St, Chicago, IL 60654",
+  logo:"https://peoplevine.blob.core.windows.net/files/521/business/6056/6056_logo.png",
+  primary_image: "https://s3-media3.fl.yelpcdn.com/bphoto/1LBmPBBz1P3KbbRar-VHKw/o.jpg", 
+  speed_of_service: 9
+  )
+restaurant.save
+
+#restaurant 8
+restaurant = Restaurant.new(
+  name: "Shake Shack", 
+  address: "66 E Ohio St, Chicago, IL 60611",
+  logo:"https://upload.wikimedia.org/wikipedia/en/thumb/7/73/Shake_Shack_Logo.svg/566px-Shake_Shack_Logo.svg.png",
+  primary_image: "https://cdn.shakeshack.com/wp-content/uploads/2014/10/shake-shack-chicago.jpg", 
+  speed_of_service: 10
+  )
+restaurant.save
+
+
 #----------------------------------------
 review = Review.new(
   value: 5, 
@@ -174,7 +216,89 @@ image = Image.new(
   )
 image.save
 
+image = Image.new(
+  name: "Chipotle 2", 
+  url: "https://b.zmtcdn.com/data/pictures/chains/6/16733306/8d8ac7ce1dfc36bad0185efe2823f08f.jpg", 
+  restaurant_id: 5
+  )
+image.save
 
+image = Image.new(
+  name: "Chipotle 3", 
+  url: "https://b.zmtcdn.com/data/reviews_photos/a4e/a8a5e682e8c63fa1137eb0a28b83aa4e_1444338864.jpg", 
+  restaurant_id: 5
+  )
+image.save
+
+image = Image.new(
+  name: "Chipotle 4", 
+  url: "https://b.zmtcdn.com/data/reviews_photos/b3e/9a98a2920737ae8c40438b09327acb3e_1444338868.jpg", 
+  restaurant_id: 5
+  )
+image.save
+
+image = Image.new(
+  name: "Taco Bell 2", 
+  url: "https://s3-media3.fl.yelpcdn.com/bphoto/DER1JyL9nw6dKXm50ibDGA/o.jpg", 
+  restaurant_id: 6
+  )
+image.save
+
+image = Image.new(
+  name: "Taco Bell 3", 
+  url: "https://timedotcom.files.wordpress.com/2017/10/taco-bell-doritos-locos.jpg", 
+  restaurant_id: 6
+  )
+image.save
+
+image = Image.new(
+  name: "Taco Bell 4", 
+  url: "http://cdn2-www.mandatory.com/assets/mandatory/legacy/2016/08/man_file_1113111_6_ranking_taco_bell_menu_items.jpeg", 
+  restaurant_id: 6
+  )
+image.save
+
+image = Image.new(
+  name: "Al's Beef 2", 
+  url: "https://s3-media4.fl.yelpcdn.com/bphoto/GZ-9h0144v-mum-GU4ovIw/o.jpg", 
+  restaurant_id: 7
+  )
+image.save
+
+image = Image.new(
+  name: "Al's Beef 3", 
+  url: "https://s3-media2.fl.yelpcdn.com/bphoto/nqY6_amaS8GsuRamwuBVOQ/o.jpg", 
+  restaurant_id: 7
+  )
+image.save
+
+image = Image.new(
+  name: "Al's Beef 4", 
+  url: "https://s3-media1.fl.yelpcdn.com/bphoto/gRtLQvJN4fBZLmnXhIiCHw/o.jpg", 
+  restaurant_id: 7
+  )
+image.save
+
+image = Image.new(
+  name: "Shake Shack 2", 
+  url: "https://s3-media3.fl.yelpcdn.com/bphoto/ajUapo20klEHHNgU0MdZaw/o.jpg", 
+  restaurant_id: 7
+  )
+image.save
+
+image = Image.new(
+  name: "Shake Shack 3", 
+  url: "https://s3-media4.fl.yelpcdn.com/bphoto/2GwcFNabWg03O5cbUppMKw/o.jpg", 
+  restaurant_id: 7
+  )
+image.save
+
+image = Image.new(
+  name: "Shake Shack 4", 
+  url: "https://s3-media4.fl.yelpcdn.com/bphoto/HvliiFcUr0RKFcnL7Ngcbw/o.jpg", 
+  restaurant_id: 7
+  )
+image.save
 #------------------------------------------------------------
 meal = Meal.new(
   description: "Big Mac, Fries, Soft Drink",
@@ -260,7 +384,7 @@ meal = Meal.new(
   description: "Chick-fil-A Chicken Biscuit, Hashbrowns, Coffee",
   restaurant_id: 4,
   upvotes: 0,
-  image: "https://www.cfacdn.com/img/order/COM/PDP_UPDATE/Images/Breakfast/Stack620_0000_CFA_1605_60_Biscuit_Chicken_PROD_2155_1240px.png"
+  image: "https://www.chick-fil-a.com/-/media/Images/CFACOM/Menu-Items/WS-Menu-PDP-Images/Breakfast/CFA_PDP_Biscuit-Chicken_1085.ashx"
   )
 meal.save
 
@@ -272,3 +396,82 @@ meal = Meal.new(
   )
 meal.save
 
+meal = Meal.new(
+  description: "Burrito Bowl, Soft Drink",
+  restaurant_id: 5,
+  upvotes: 0,
+  image: "http://www.couponing101.com/wp-content/uploads/2016/10/6358737839538413184635822_Chipotle_Chicken_Salad_With_Vinaigrette_2x.jpg"
+  )
+meal.save
+
+meal = Meal.new(
+  description: "Burrito, Soft Drink",
+  restaurant_id: 5,
+  upvotes: 0,
+  image: "https://cmt.azureedge.net/media/orig_chipotle-steak-burrito-20161121132850895294xx8ol.jpg"
+  )
+meal.save
+
+meal = Meal.new(
+  description: "Tacos, Soft Drink",
+  restaurant_id: 5,
+  upvotes: 0,
+  image: "https://d5bzqyuki558t.cloudfront.net/cms-assets/system/asset_versions/attachments/000/000/308/original/_0002_CrispyCornTaco.png"
+  )
+meal.save
+
+meal = Meal.new(
+  description: "Crunchwrap Supreme, Soft Drink",
+  restaurant_id: 6,
+  upvotes: 0,
+  image: "https://www.tacobell.com/images/22362_crunchwrap_supreme_269x269.jpg"
+  )
+meal.save
+
+meal = Meal.new(
+  description: "Crunchwrap Supreme, Soft Drink",
+  restaurant_id: 6,
+  upvotes: 0,
+  image: "https://www.tacobell.com/images/22362_crunchwrap_supreme_269x269.jpg"
+  )
+meal.save
+
+meal = Meal.new(
+  description: "3 Doritos Locos Tacos, Soft Drink",
+  restaurant_id: 6,
+  upvotes: 0,
+  image: "https://www.tacobell.com/images/22701_11._3_doritos_locos_tacos_supreme_combo_269x269.jpg"
+  )
+meal.save
+
+meal = Meal.new(
+  description: "Regular Beef, Fresh Cut Fries, Soft Drink",
+  restaurant_id: 7,
+  upvotes: 0,
+  image: "https://s3-media3.fl.yelpcdn.com/bphoto/_YqIW2gIswMMV9o5Sqq5tw/o.jpg"
+  )
+meal.save
+
+meal = Meal.new(
+  description: "Original Polish, Fresh Cut Fries, Soft Drink",
+  restaurant_id: 7,
+  upvotes: 0,
+  image: "https://s3-media2.fl.yelpcdn.com/bphoto/poI0mm0I5SdC9jA7RzZX6Q/o.jpg"
+  )
+meal.save
+
+meal = Meal.new(
+  description: "Original Dog, Fresh Cut Fries, Soft Drink",
+  restaurant_id: 7,
+  upvotes: 0,
+  image: "https://s3-media4.fl.yelpcdn.com/bphoto/qASywWixUofrVPrA6PBdCw/o.jpg"
+  )
+meal.save
+
+meal = Meal.new(
+  description: "ShackBurger, Fries, Soft Drink",
+  restaurant_id: 8,
+  upvotes: 0,
+  image: "https://fortunedotcom.files.wordpress.com/2016/09/514078498.jpg"
+  )
+meal.save
